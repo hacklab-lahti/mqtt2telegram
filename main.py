9e6b645f174fa181ec2658bd9b5ea8c130e98243
Timo Häkli <timo.hakli@kiinteistotehdas.fi>
2019-05-20 23:15:16 +0300

    changed requests to asyncio an aiohttp

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import paho.mqtt.subscribe as subscribe
-import requests
+import asyncio
+import aiohttp
+import urllib
 
 import settings
 
 def send_to_telegram(text, silent=settings.TELEGRAM_DISABLE_NOTIFICATION):
-    url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&disable_notification={}".format(settings.TELEGRAM_BOT_TOKEN, settings.TELEGRAM_CHAT_ID, text, silent)
-    requests.get(url)
+    asyncio.ensure_future(_send_to_telegram(**args, **kwargs))
+
+async def _send_to_telegram(text, silent=settings.TELEGRAM_DISABLE_NOTIFICATION):
+    text = urllib.parse.quote(text)
+    url = ("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&disable_notification={}".format(settings.TELEGRAM_BOT_TOKEN, settings.TELEGRAM_CHAT_ID, text, silent))
+    async with aiohttp.ClientSession() as session:
+                async with session.get(url, timeout=10) as resp:
+                    print("Telegram message sent, response from server: {}".format(await resp.text()))
 
 def on_mqtt_received(client, userdata, message):
     print("%s %s" % (message.topic, message.payload))

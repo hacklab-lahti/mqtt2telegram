9c58127ca861bfdcdfa23250474c054ba4045515
Timo Häkli <timo.hakli@kiinteistotehdas.fi>
2019-05-20 22:35:25 +0300

    initial import

---
+++ main.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as subscribe
+import requests
+
+import settings
+
+def send_to_telegram(text, silent=settings.TELEGRAM_DISABLE_NOTIFICATION):
+    url = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&disable_notification={}".format(settings.TELEGRAM_BOT_TOKEN, settings.TELEGRAM_CHAT_ID, text, silent)
+    requests.get(url)
+
+def on_mqtt_received(client, userdata, message):
+    print("%s %s" % (message.topic, message.payload))
+    send_to_telegram("{} {}".format(message.topic, message.payload.decode('utf-8')))
+
+mqttc = mqtt.Client()
+
+subscribe.callback(on_mqtt_received, settings.MQTT_TOPICS, qos=0, userdata=None, hostname="localhost",
+    port=1883, client_id="", keepalive=60, will=None, auth=None, tls=None,
+    protocol=mqtt.MQTTv311)
---
+++ settings-example.py
@@ -0,0 +1,5 @@
+MQTT_TOPICS = ["kolava/#", "renksu/#"]
+
+TELEGRAM_BOT_TOKEN = "" # Ask your Bot API Token from BotFather
+TELEGRAM_CHAT_ID = "" # grab from getUpdates
+TELEGRAM_DISABLE_NOTIFICATION = "true"
